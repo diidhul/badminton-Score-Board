5e7795cec108abf9fd95ded16709b9333887affa
diidhul <fadhil.elsafer@gmail.com>
2024-07-20 21:13:57 +0700

    fixing all functions and the style of element

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const increaseBtn = document.getElementById("increaseBtn");
 const decreaseBtn = document.getElementById("decreaseBtn");
+const increaseBtn2 = document.getElementById("increaseBtn2");
+const decreaseBtn2 = document.getElementById("decreaseBtn2");
 const resetBtn = document.getElementById("resetBtn");
 const countLabel = document.getElementById("countLabel");
+const countLabel2 = document.getElementById("countLabel2");
 
 let count = 0;
+let count2 = 0;
 
 increaseBtn.onclick = function () {
   count++;
@@ -15,7 +19,19 @@ decreaseBtn.onclick = function () {
   countLabel.textContent = count;
 };
 
+increaseBtn2.onclick = function () {
+  count2++;
+  countLabel2.textContent = count2;
+};
+
+decreaseBtn2.onclick = function () {
+  count2--;
+  countLabel2.textContent = count2;
+};
+
 resetBtn.onclick = function () {
   count = 0;
+  count2 = 0;
   countLabel.textContent = count;
+  countLabel2.textContent = count2;
 };
